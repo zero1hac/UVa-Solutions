b461be4903fc7ea76119dc00ce18198a678328e2
zero1hac <dushyant.kumar.mat13@iitbhu.ac.in>
2016-06-17 23:02:28 +0530

    10141 added

--- 10141.cpp
+++ 10141.cpp
@@ -1,14 +1,52 @@
-/* zeroonehacker
+/*zeroonehacker
  * Dushyant Kumar Mishra
+ * Indian Institute of Technology Varnasi
  * Mathematics and Computing Part III
- * Indian Institute of Technology Varanasi
+ *
  */
-#include <iostream>
-#include <string>
-#include <cmath>
 #include <cstdio>
+#include <cstring>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+	int n, p;
+	string x;
+	int count = 0;
+	while (scanf("%d%d",&n,&p), n || p) {
+		count++;
+		double price = 2000000000, q;
+		int req = -1, rm;
+		string name, temp;
+
+		for (int i = 0; i < n; i++) {
+			cin.ignore();
+			getline(cin, x);
+		}
+
+		while (p--) {
+			getline(cin,temp);
+			scanf("%lf %d\n", &q, &rm);
+			if (rm > req) {
+				req = rm;
+				name = temp;
+				price = q;
+			} else if (rm == req && p < price) {
+				req = rm;
+				name = temp;
+				price = q;
+			}
+
+			for (int i = 0; i < rm; i++) {
+				getline(cin, x);
+			}
+		}
+
+		if (count > 1)
+			cout << endl;
+		cout << "RFP #" << count << endl;
+		cout << name << endl;
+	}
+
 	return 0;
 }
